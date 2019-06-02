37637bdcacf1d8cd13900fa66ace8f7cd59dc645
agonzalezgarz <a.gonzalezgarz@alumnos.urjc.es>
2019-06-02 10:08:13 +0200

    Arreglado

--- src/test/java/es/codeurjc/ais/tictactoe/TicTacToeGameTest.java
+++ src/test/java/es/codeurjc/ais/tictactoe/TicTacToeGameTest.java
@@ -1,6 +1,6 @@
 package es.codeurjc.ais.tictactoe;
 
-import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.reset;
 import static org.mockito.Mockito.times;
