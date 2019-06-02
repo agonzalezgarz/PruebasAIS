85e948f1da6bb9e1a05bf610f0061667949dfbef
agonzalezgarz <a.gonzalezgarz@alumnos.urjc.es>
2019-06-02 16:11:55 +0200

    Cambiado

---
+++ src/test/java/es/codeurjc/ais/tictactoe/TestBoard.java
@@ -0,0 +1,166 @@
+package es.codeurjc.ais.tictactoe;
+
+import static org.junit.Assert.*;
+
+
+import org.junit.Test;
+
+public class TestBoard{
+		
+	// Crear tablero 
+	
+	 Board tablero = new Board();
+		
+	// Creamos jugadores
+	 
+	Player jugadorUno = new Player(1, "x", "Jugador Uno");
+	Player jugadorDos = new Player(2, "o", "Jugador Dos");
+		
+
+	@Test
+	public void testJugadorUnoWinner() {
+		
+		//Simulamos una partida
+		
+		// Posicion ganadora esperada
+		
+		int[] posicion = {0, 1, 2};
+		
+		// {0, 1, 2} es una posicion ganadora		
+		
+		//Jugador Uno -> Una X en la posicion 0
+		
+		tablero.getCell(0).value = jugadorUno.getLabel();
+		
+		//Jugador Dos -> Un O en la posicion 3
+		
+		tablero.getCell(3).value = jugadorDos.getLabel();
+		
+		//Jugador Uno -> Un X en la posicion 1
+		
+		tablero.getCell(1).value = jugadorUno.getLabel();
+		
+		//Jugador Dos -> Un O en la posicion
+		
+		tablero.getCell(4).value = jugadorDos.getLabel();
+		
+		//Jugador Uno -> Un X en la posicion 2
+		
+		tablero.getCell(2).value = jugadorUno.getLabel();
+		
+		
+		// Comprobamos que la posicion ganadora esperada es igual a la obtenida
+		
+		assertArrayEquals(posicion, tablero.getCellsIfWinner(jugadorUno.getLabel()));
+		
+		//Comprobamos si el jugador dos no tiene la posicion ganadora
+		
+		assertNull(tablero.getCellsIfWinner(jugadorDos.getLabel()));
+		
+		//Comprobamos que la partida no es un empate
+		
+		assertEquals(false, tablero.checkDraw());		
+	}
+	
+	@Test
+	public void testJugadorDosWinner() {
+		
+		//Simulamos una partida
+		
+		// Posicion ganadora esperada
+		
+		int[] posicion = {3, 4, 5};
+		
+		//Jugador Uno -> Una X en la posicion 0
+		
+		tablero.getCell(0).value = jugadorUno.getLabel();
+		
+		//Jugador Dos -> Un O en la posicion 3
+		
+		tablero.getCell(3).value = jugadorDos.getLabel();
+		
+		//Jugador Uno -> Un X en la posicion 1
+		
+		tablero.getCell(1).value = jugadorUno.getLabel();
+		
+		//Jugador Dos -> Un O en la posicion 4
+		
+		tablero.getCell(4).value = jugadorDos.getLabel();
+		
+		//Jugador Uno -> Un X en la posicion 6
+		
+		tablero.getCell(6).value = jugadorUno.getLabel();
+		
+		//Jugador Dos -> Un O en la posicion 5
+		
+		tablero.getCell(5).value = jugadorDos.getLabel();	
+		
+		
+		// Comprobamos que la posicion ganadora esperada es igual a la obtenida
+		
+		assertArrayEquals(posicion, tablero.getCellsIfWinner(jugadorDos.getLabel()));
+		
+		//Comprobamos si el jugador uno no tiene la posicion ganadora
+		
+		assertNull(tablero.getCellsIfWinner(jugadorUno.getLabel()));
+		
+		//Comprobamos que la partida no es un empate
+		
+		assertEquals(false, tablero.checkDraw());	
+		
+	}
+	
+	@Test 
+	public void testJuegoEmpatado() {
+		
+		
+		//Simulamos una partida
+		
+		assertFalse(tablero.checkDraw());
+		
+		//Jugador Uno -> Una X en la posicion 0
+		
+		tablero.getCell(0).value = jugadorUno.getLabel();
+		
+		//Jugador Dos -> Un O en la posicion 3
+		
+		tablero.getCell(3).value = jugadorDos.getLabel();
+		
+		//Jugador Uno -> Un X en la posicion 1
+		
+		tablero.getCell(1).value = jugadorUno.getLabel();
+		
+		//Jugador Dos -> Un O en la posicion 4
+		
+		tablero.getCell(4).value = jugadorDos.getLabel();
+		
+		//Jugador Uno -> Un X en la posicion 5
+		
+		tablero.getCell(5).value = jugadorUno.getLabel();
+		
+		//Jugador Dos -> Un O en la posicion 2
+		
+		tablero.getCell(2).value = jugadorDos.getLabel();
+			
+		//Jugador Uno -> Un X en la posicion 6
+
+		tablero.getCell(6).value = jugadorUno.getLabel();
+		
+		//Jugador Dos -> Un O en la posicion 7
+
+		tablero.getCell(7).value = jugadorDos.getLabel();
+
+		// Jugador Uno -> Un X en la posicion8
+		
+		tablero.getCell(8).value = jugadorUno.getLabel();
+		
+		// Ahora si seria un empate
+		
+		assertNull(tablero.getCellsIfWinner(jugadorUno.getLabel()));
+		
+		assertNull(tablero.getCellsIfWinner(jugadorDos.getLabel()));
+		
+		assertTrue(tablero.checkDraw());
+		
+	}
+}
---
+++ src/test/java/es/codeurjc/ais/tictactoe/TestSistemasAplicacion.java
@@ -0,0 +1,128 @@
+package es.codeurjc.ais.tictactoe;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+
+public class TestSistemasAplicacion{
+
+	// Creamos el objeto WebDriver especifico para el navegador
+	// que queramos utilizar
+	
+	protected WebDriver driver1, driver2;
+	
+	@BeforeClass
+	public static void setupClass() {
+		WebDriverManager.firefoxdriver().setup();		
+		WebApp.start();
+	}
+	
+	@AfterClass
+	public static void teardownClass() {
+		WebApp.stop();
+	}
+	
+	//LO ejecutamos antes de cada test
+	@Before
+	public void setup() {
+		
+		driver1 = new FirefoxDriver();
+		driver2 = new FirefoxDriver();
+		
+		WebDriverWait wait1 = new WebDriverWait(driver1, 50);
+		WebDriverWait wait2 = new WebDriverWait(driver2, 50);
+		
+		driver1.get("http://localhost:8081");
+		driver2.get("http://localhost:8081");
+		
+		
+		wait1.until(ExpectedConditions.elementToBeClickable(By.id("nickname")));
+		wait2.until(ExpectedConditions.elementToBeClickable(By.id("nickname")));
+		wait1.until(ExpectedConditions.elementToBeClickable(By.id("startBtn")));
+		wait2.until(ExpectedConditions.elementToBeClickable(By.id("startBtn")));
+
+
+		driver1.findElement(By.id("nickname")).sendKeys("JugadorUno");
+		driver1.findElement(By.id("startBtn")).click();
+
+		driver2.findElement(By.id("nickname")).sendKeys("JugadorDos");
+		driver2.findElement(By.id("startBtn")).click();
+		
+	}
+
+	// Al finalizar el test se cierra el browser con el metodo quit()
+	@After
+	public void teardown() {
+		if(driver1 != null) {
+			driver1.quit();
+		}
+		if(driver2 != null) {
+			driver2.quit();
+		}
+	}
+	
+	// Localizamos los elementos en la pagina e interactuamos con ellos (click)
+	
+	@Test
+	public void testJugadorUnoWinner() throws InterruptedException{
+		
+		
+		driver1.findElement(By.id("cell-0")).click();
+		driver2.findElement(By.id("cell-3")).click();
+		driver1.findElement(By.id("cell-1")).click();
+		driver2.findElement(By.id("cell-4")).click();
+		driver1.findElement(By.id("cell-2")).click();
+
+		assertEquals(driver1.switchTo().alert().getText(), "JugadorUno wins! JugadorDos looses.");
+		assertEquals(driver2.switchTo().alert().getText(), "JugadorUno wins! JugadorDos looses.");
+
+	}
+	
+	@Test
+	public void testJugadorDosWinner() throws InterruptedException{
+	
+		
+
+		driver1.findElement(By.id("cell-0")).click();
+		driver2.findElement(By.id("cell-3")).click();
+		driver1.findElement(By.id("cell-1")).click();
+		driver2.findElement(By.id("cell-4")).click();
+		driver1.findElement(By.id("cell-6")).click();
+		driver2.findElement(By.id("cell-5")).click();
+
+		assertEquals(driver2.switchTo().alert().getText(), "JugadorDos wins! JugadorUno looses.");
+		assertEquals(driver1.switchTo().alert().getText(), "JugadorDos wins! JugadorUno looses.");
+
+	}
+	
+	@Test
+	public void testPartidaEmpatada() throws InterruptedException{
+		
+
+		
+		driver1.findElement(By.id("cell-0")).click();
+		driver2.findElement(By.id("cell-3")).click();
+		driver1.findElement(By.id("cell-1")).click();
+		driver2.findElement(By.id("cell-4")).click();
+		driver1.findElement(By.id("cell-5")).click();
+		driver2.findElement(By.id("cell-2")).click();
+		driver1.findElement(By.id("cell-6")).click();
+		driver2.findElement(By.id("cell-7")).click();
+		driver1.findElement(By.id("cell-8")).click();
+		
+		assertEquals(driver1.switchTo().alert().getText(), "Draw!");
+		assertEquals(driver2.switchTo().alert().getText(), "Draw!");
+	}
+}
